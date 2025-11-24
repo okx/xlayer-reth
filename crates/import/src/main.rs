5c09cab720d9651510d12f9566574be63343f6b6
Dumi Loghin <dumiloghin@gmail.com>
2025-11-24 17:37:50 +0800

    feature import ported from reth

---
+++ crates/import/src/main.rs
@@ -0,0 +1,35 @@
+#![allow(missing_docs, rustdoc::missing_crate_level_docs)]
+
+use clap::Parser;
+use eyre::Result;
+use reth_optimism_cli::chainspec::OpChainSpecParser;
+use reth_optimism_consensus::OpBeaconConsensus;
+use reth_optimism_evm::{OpEvmConfig, OpRethReceiptBuilder};
+use reth_optimism_node::OpNode;
+use std::sync::Arc;
+use tracing::info;
+
+mod import;
+use import::ImportCommand;
+
+pub const XLAYER_RETH_CLIENT_VERSION: &str = concat!("xlayer/v", env!("CARGO_PKG_VERSION"));
+
+#[global_allocator]
+static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize tracing/logging
+    reth_cli_util::sigsegv_handler::install();
+
+    info!(target: "xlayer::import", "XLayer Reth Import starting");
+
+    let cmd = ImportCommand::<OpChainSpecParser>::parse();
+
+    cmd.execute::<OpNode, _>(|chain_spec| {
+        let evm_config = OpEvmConfig::new(chain_spec.clone(), OpRethReceiptBuilder::default());
+        let consensus = Arc::new(OpBeaconConsensus::new(chain_spec));
+        (evm_config, consensus)
+    })
+    .await
+}
