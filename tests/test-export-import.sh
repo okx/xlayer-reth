1c2bdd8fcf89671bf6d53e0d46876817aaf1a363
Dumi Loghin <dumiloghin@gmail.com>
2025-11-25 19:08:43 +0800

    improve test by checking export->import->export diff

--- tests/test-export-import.sh
+++ tests/test-export-import.sh
@@ -18,9 +18,18 @@ tar xf op-reth-seq.tar.xz
 # Export the blocks
 xlayer-reth-export --datadir op-reth-seq --chain genesis-reth.json --exported-data exp-test-78.bin --start-block 8593921 --end-block 8593999 | tee export-78.log
 xlayer-reth-export --datadir op-reth-seq --chain genesis-reth.json --exported-data exp-test-all.bin | tee export-all.log
-echo "*** Done exporting."
+echo "✅ Done exporting."
 
 # Import the blocks
 xlayer-reth-import --datadir data --chain genesis-reth.json --exported-data exp-test-78.bin | tee import-78.log
 xlayer-reth-import --datadir data --chain genesis-reth.json --exported-data exp-test-all.bin | tee import-all.log
-echo "*** Done importing."
+echo "✅ Done importing."
+
+# Export and check
+xlayer-reth-export --datadir data --chain genesis-reth.json --exported-data exp-test-all-2.bin
+diff -q exp-test-all.bin exp-test-all-2.bin
+if [ $? -ne 0 ]; then
+    echo "❌ Export and check failed."
+    exit 1
+fi
+echo "✅ Export and check passed."
