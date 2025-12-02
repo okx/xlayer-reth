d2a26ff4e2c1382a6e6859faf5c7c26fca8c2a62
Dumi Loghin <dumiloghin@gmail.com>
2025-12-02 11:42:18 +0800

    move import and export to bin/tools, update tests and readme

---
+++ crates/export/src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(missing_docs, rustdoc::missing_crate_level_docs)]
+
+mod export;
+pub use export::ExportCommand;
+
---
+++ crates/import/src/lib.rs
@@ -0,0 +1,5 @@
+#![allow(missing_docs, rustdoc::missing_crate_level_docs)]
+
+mod import;
+pub use import::ImportCommand;
+
