db4437a3284e1a88e5d3d2450e10cb00ea3b96fd
Vui-Chee <vuicheesiew@gmail.com>
2025-12-31 14:57:51 +0800

    initial versioning crate

---
+++ crates/version/build.rs
@@ -0,0 +1,73 @@
+use std::{env, error::Error};
+use vergen::{BuildBuilder, CargoBuilder, Emitter};
+use vergen_git2::Git2Builder;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut emitter = Emitter::default();
+
+    let build_builder = BuildBuilder::default().build_timestamp(true).build()?;
+    emitter.add_instructions(&build_builder)?;
+
+    let cargo_builder = CargoBuilder::default().features(true).target_triple(true).build()?;
+    emitter.add_instructions(&cargo_builder)?;
+
+    let git_builder =
+        Git2Builder::default().describe(false, true, None).dirty(true).sha(false).build()?;
+    emitter.add_instructions(&git_builder)?;
+
+    emitter.emit_and_set()?;
+
+    let sha = env::var("VERGEN_GIT_SHA")?;
+    let sha_short = &sha[0..7];
+
+    let is_dirty = env::var("VERGEN_GIT_DIRTY")? == "true";
+    // > git describe --always --tags
+    // if not on a tag: v0.2.0-beta.3-82-g1939939b
+    // if on a tag: v0.2.0-beta.3
+    let not_on_tag = env::var("VERGEN_GIT_DESCRIBE")?.ends_with(&format!("-g{sha_short}"));
+    let version_suffix = if is_dirty || not_on_tag { "-dev" } else { "" };
+    println!("cargo:rustc-env=RETH_VERSION_SUFFIX={version_suffix}");
+
+    // Set short SHA
+    println!("cargo:rustc-env=VERGEN_GIT_SHA_SHORT={}", &sha[..8]);
+
+    // Set the build profile
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let profile = out_dir.rsplit(std::path::MAIN_SEPARATOR).nth(3).unwrap();
+    println!("cargo:rustc-env=RETH_BUILD_PROFILE={profile}");
+
+    // Set formatted version strings
+    let pkg_version = env!("CARGO_PKG_VERSION");
+
+    // The short version information for reth.
+    // - The latest version from Cargo.toml
+    // - The short SHA of the latest commit.
+    // Example: 0.1.0 (defa64b2)
+    println!("cargo:rustc-env=RETH_SHORT_VERSION={pkg_version}{version_suffix} ({sha_short})");
+
+    // LONG_VERSION
+    //
+    // The long version information for reth.
+    //
+    // Example:
+    //
+    // ```text
+    // XLayer Reth Commit SHA: 0.1.0-dev (3c0647e)
+    // Upstream Reth Version: 3ba1e5d3
+    // Build Timestamp: 2023-05-19T01:47:19.815651705Z
+    // Build Features: jemalloc
+    // Build Profile: maxperf
+    // ```
+    println!("cargo:rustc-env=RETH_LONG_VERSION_0=XLayer Reth Version: {pkg_version}{version_suffix} ({sha_short})");
+    println!(
+        "cargo:rustc-env=RETH_LONG_VERSION_1=Build Timestamp: {}",
+        env::var("VERGEN_BUILD_TIMESTAMP")?
+    );
+    println!(
+        "cargo:rustc-env=RETH_LONG_VERSION_2=Build Features: {}",
+        env::var("VERGEN_CARGO_FEATURES")?
+    );
+    println!("cargo:rustc-env=RETH_LONG_VERSION_3=Build Profile: {profile}");
+
+    Ok(())
+}
---
+++ crates/version/src/lib.rs
@@ -0,0 +1,65 @@
+use reth::version::{
+    default_reth_version_metadata, try_init_version_metadata, RethCliVersionConsts,
+};
+
+pub const XLAYER_RETH_CLIENT_VERSION: &str = concat!("xlayer/v", env!("CARGO_PKG_VERSION"));
+
+pub fn init_version_metadata() {
+    // NOTE: these versions are the upstream repo default values.
+    let default_version_metadata = default_reth_version_metadata();
+
+    let upstream_version =
+        format!("Upstream Reth Version: {}", default_version_metadata.vergen_git_sha);
+    let long_version = format!(
+        "{}\n{}\n{}\n{}\n{}",
+        env!("RETH_LONG_VERSION_0"),
+        upstream_version,
+        env!("RETH_LONG_VERSION_1"),
+        env!("RETH_LONG_VERSION_2"),
+        env!("RETH_LONG_VERSION_3"),
+    );
+
+    try_init_version_metadata(RethCliVersionConsts {
+        name_client: "XLayer Reth".to_string().into(),
+        cargo_pkg_version: format!(
+            "{}/{}",
+            default_version_metadata.cargo_pkg_version,
+            env!("CARGO_PKG_VERSION")
+        )
+        .into(),
+        p2p_client_version: format!(
+            "{}/{}",
+            default_version_metadata.p2p_client_version, XLAYER_RETH_CLIENT_VERSION
+        )
+        .into(),
+        extra_data: format!(
+            "{}/{}",
+            default_version_metadata.extra_data, XLAYER_RETH_CLIENT_VERSION
+        )
+        .into(),
+        vergen_git_sha: env!("VERGEN_GIT_SHA_SHORT").into(),
+        vergen_git_sha_long: env!("VERGEN_GIT_SHA").into(),
+        short_version: env!("RETH_SHORT_VERSION").into(),
+        long_version: long_version.into(),
+        ..default_version_metadata
+    })
+    .expect("Unable to init version metadata");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::init_version_metadata;
+    use reth_node_core::version::version_metadata;
+
+    #[test]
+    fn verify_version() {
+        init_version_metadata();
+        let version_output = version_metadata();
+
+        println!("Short version: {:?}", version_output.short_version);
+        println!("Long version: {:?}", version_output.long_version);
+
+        // TODO: verify as binary.
+        // test --version prints correct version
+    }
+}
