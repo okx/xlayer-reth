4c7d463f691e1a6f29b35f6b6ccbec728a50d756
Vui-Chee <vuicheesiew@gmail.com>
2025-12-31 16:08:16 +0800

    nuke redundant comments

--- crates/version/build.rs
+++ crates/version/build.rs
@@ -21,9 +21,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let sha_short = &sha[0..7];
 
     let is_dirty = env::var("VERGEN_GIT_DIRTY")? == "true";
-    // > git describe --always --tags
-    // if not on a tag: v0.2.0-beta.3-82-g1939939b
-    // if on a tag: v0.2.0-beta.3
     let not_on_tag = env::var("VERGEN_GIT_DESCRIBE")?.ends_with(&format!("-g{sha_short}"));
     let version_suffix = if is_dirty || not_on_tag { "-dev" } else { "" };
     println!("cargo:rustc-env=RETH_VERSION_SUFFIX={version_suffix}");
@@ -39,25 +36,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Set formatted version strings
     let pkg_version = env!("CARGO_PKG_VERSION");
 
-    // The short version information for reth.
-    // - The latest version from Cargo.toml
-    // - The short SHA of the latest commit.
-    // Example: 0.1.0 (defa64b2)
     println!("cargo:rustc-env=RETH_SHORT_VERSION={pkg_version}{version_suffix} ({sha_short})");
 
-    // LONG_VERSION
-    //
-    // The long version information for reth.
-    //
-    // Example:
-    //
-    // ```text
-    // xlayer-reth-node Commit SHA: 0.1.0-dev (3c0647e)
-    // Upstream Reth Version: 3ba1e5d3
-    // Build Timestamp: 2023-05-19T01:47:19.815651705Z
-    // Build Features: jemalloc
-    // Build Profile: maxperf
-    // ```
     println!(
         "cargo:rustc-env=RETH_LONG_VERSION_0=Version: {pkg_version}{version_suffix} ({sha_short})"
     );
