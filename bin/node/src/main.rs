71769eec6508b53b56d760252cfd1ce7a4dc209c
Niven <sieniven@gmail.com>
2026-01-08 10:24:06 +0800

    chore(flashblocks): fix flashblocks rpc service logging level on init

--- bin/node/src/main.rs
+++ bin/node/src/main.rs
@@ -7,7 +7,7 @@ use std::sync::Arc;
 
 use args_xlayer::XLayerArgs;
 use clap::Parser;
-use tracing::{error, info, warn};
+use tracing::{error, info, trace};
 
 use op_rbuilder::{
     args::OpRbuilderArgs,
@@ -194,7 +194,7 @@ fn main() {
                             service.spawn();
                             info!(target: "reth::cli", "xlayer flashblocks service initialized");
                         } else {
-                            warn!(target: "reth::cli", "unable to get flashblock receiver, xlayer flashblocks service not initialized");
+                            trace!(target: "reth::cli", "unable to get flashblock receiver, xlayer flashblocks service not initialized");
                         }
 
                         if let Some(pending_blocks_rx) = new_op_eth_api.pending_block_rx() {
@@ -212,7 +212,7 @@ fn main() {
                             )?;
                             info!(target: "reth::cli", "xlayer eth pubsub initialized");
                         } else {
-                            warn!(target: "reth::cli", "unable to get pending blocks receiver, flashblocks eth pubsub not replaced");
+                            trace!(target: "reth::cli", "unable to get pending blocks receiver, flashblocks pubsub not initialized");
                         }
 
                         // Register XLayer RPC
