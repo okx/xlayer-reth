2afcc32d9a4da7da43b1d2f9d6797d0fb0f13364
lucas.lim <lucas.lim@okg.com>
2026-02-27 13:21:00 +0800

    fix: remove redundant code after merge

--- crates/builder/src/builders/flashblocks/config.rs
+++ crates/builder/src/builders/flashblocks/config.rs
@@ -97,7 +97,7 @@ impl Default for FlashblocksConfig {
             p2p_process_full_payload: false,
             ws_subscriber_limit: None,
             enable_async_trie_precalc: false,
-            async_trie_precalc_start_flashblock: 0,
+            async_trie_precalc_start_flashblock: 1,
         }
     }
 }
